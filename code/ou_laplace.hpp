ea7bad1d10223f093c8e799879266cfe735d2a22
Wanxin Li <wanxin.li@uwaterloo.ca>
2020-04-08 12:40:41 -0400

    added ou_nll.hpp, ou_laplace.hpp

--- code/ou_laplace.hpp
+++ code/ou_laplace.hpp
@@ -1,71 +1,7 @@
-
 #include <laplace.hpp>
-
-/* The following is (almost) copy-pasted from the 'spatial' example */
-template<class Type>
-struct joint_nll {
-  /* Data and parameter objects for spatial example: */
-  vector<Type> y;
-  matrix<Type> X;
-  matrix<Type> dd;
-  vector<Type> b;
-  Type a;
-  Type log_sigma;
-  
-  /* Constructor */
-  joint_nll(vector<Type> y_,
-            matrix<Type> X_,
-            matrix<Type> dd_,
-            vector<Type> b_,
-            Type a_,
-            Type log_sigma_) :
-    y(y_), X(X_), dd(dd_), b(b_),
-    a(a_), log_sigma(log_sigma_) {}
-  
-  /* Evaluate the negative joint log-likelihood as function of the
-  random effects */
-  template <typename T>
-  T operator()(vector<T> u) {
-    int n = u.size();
-    T res=0;
-    vector<T> eta = T(exp(log_sigma)) * u;
-    vector<Type> mu = X * b;
-    eta = eta + mu.template cast<T>();
-    matrix<T> cov(n,n); 
-    for (int i=0; i<n; i++)
-    {
-      cov(i,i) = 1.0;
-      for (int j=0; j<i; j++)
-      {
-        // Exponentially decaying correlation
-        cov(i,j) = exp(-a * dd(i,j));
-        cov(j,i) = cov(i,j);
-      }
-    }
-    density::MVNORM_t<T> neg_log_density(cov);
-    res += neg_log_density(u);
-    // logdpois = N log lam - lam
-    for(int i=0; i<y.size(); i++)
-      res -= T(y[i]) * eta[i] - exp(eta[i]);
-    return res;
-  }
-};
-
-
-template<class Type>
-Type objective_function<Type>::operator() ()
-{
-  // DATA_VECTOR(y);
-  // DATA_MATRIX(X);
-  // DATA_MATRIX(dd);
-  // PARAMETER_VECTOR(b);
-  // PARAMETER(a);
-  // PARAMETER(log_sigma);
-  // int n = dd.rows();
   
   // data inputs
-  DATA_MATRIX(H); // rate covariate matrix
-  DATA_VECTOR(X); // donor-acceptor distance,underlying latent random variable
+  DATA_SCALAR(x0); 
   DATA_SCALAR(dt); //  Time gap
   DATA_SCALAR(b0); // beta 0
   DATA_SCALAR(b1); // beta 1
@@ -77,16 +13,15 @@ Type objective_function<Type>::operator() ()
   PARAMETER(sigma); // parameter estimated
   
   // Construct joint negative log-likelihood
-  joint_nll<Type> jnll(y, X, dd, b, a, log_sigma);
+  joint_nll<Type> jnll(x0, dt, b0, b1, y, gamma, mu, sigma);
   
-  // Random effect initial guess
-  vector<Type> u(n);
+  int n = y.size()
+  vector<Type> X(n);
   X.setZero();
   
-  // Calculate Laplace approx (updates u)
   DATA_INTEGER(niter);
-  Type res = laplace(jnll, u, niter);
-  ADREPORT(u)
+  Type res = laplace(jnll, X, niter);
+  ADREPORT(X)
+  
     
-    return res;
-}
+  return res;
--- code/ou_nll.hpp
+++ code/ou_nll.hpp
@@ -1,27 +1,68 @@
-#include <Rcpp.h>
-using namespace Rcpp;
+/* The following is (almost) copy-pasted from the 'spatial' example */
+template<class Type>
+struct joint_nll {
+  
+  /* Data and parameter objects for spatial example: */
+  vector<Type> y;
+  matrix<Type> X;
+  matrix<Type> dd;
+  vector<Type> b;
+  Type a;
+  Type log_sigma;
+  
+  // data inputs
+  Type x0; 
+  Type dt; //  Time gap
+  Type b0; // beta 0
+  Type b1; // beta 1
+  vector<Type> y; // number of photons recorded
+  
+  // Parameter inputs
+  Type gamma; // number of photons recorded
+  Type mu; // parameter estimated
+  Type sigma; // parameter estimated
+  
+  joint_nll<Type> jnll(x0, dt, b0, b1, y, gamma, mu, sigma);
+  
+  /* Constructor */
+  joint_nll(Type x0_,
+            Type dt_,
+            Type b0_,
+            Type b1_,
+            vector<Type> y_,
+            Type gamma_,
+            Type mu_,
+            Type sigma_):
+  x0(x0_), dt(dt_), b0(b0_), b1(b1_),
+  y(y_), gamma(gamma_), mu(mu_), sigma(sigma_) {}
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
+  
+  /* Evaluate the negative joint log-likelihood as function of the
+  random effects */
+ Type operator()(vector<Type> X) {
 
-// [[Rcpp::export]]
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
-}
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
+   //This compute the Omega value
+   Type omega = exp(-gamma*dt);
+   //This compute the tao value
+   Type tao = sigma/sqrt(2*gamma);
+   
+   int N = X.size();
+   REPORT(N);
+   //this computes X * b1 + b0
+   vector<Type> x_beta1 = -X*b1 +b0;
+   REPORT(x_beta1);
+   // This computes (gamma_n*(X * b1 + b0)-exp(X * b1 + b0))
+   Type sum_gamma = (y* x_beta1 -exp(x_beta1)).sum();
+   REPORT(sum_gamma);
+   
+   Type f; //= sum_gamma;// Define variable that holds the return value
+   
+   for(int i=1;i<N;i++) {
+     // Calculate mu and sigma for OU process
+     Type mu_ou = mu+omega*(X[i-1]-mu);
+     Type sigma_ou = tao*sqrt((1-omega*omega));
+     f =  f  + dnorm(X[i],mu_ou,sigma_ou,true); // todo: check later
+   }
+   return -f; //negative loglikelihood
+  }
+};
