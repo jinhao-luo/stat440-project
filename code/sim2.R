48e43ee6cceb225033755817fec8aeccefe3110c
Wanxin Li <wanxin.li@uwaterloo.ca>
2020-04-12 11:51:37 -0400

    updated sim2.R

--- code/sim2.R
+++ code/sim2.R
@@ -70,17 +70,18 @@ sim_2 <- function(from, beta0 = 20, beta1 = .5, gamma = 1, mu = 10, sigma, dt = 
     models
 }
 
-test_cases <- data.frame(sigma=c(0.00000001, 0.001, 0.01, 0.1, 1, sqrt(2), sqrt(5)))
+test_cases <- data.frame(sigma=c(0.00000001, 0.001, 0.01, 0.1, 1, 1.1, 1.2, 1.3, 1.4, 1.5))
 ou_result <- apply(test_cases, 1, function(info) {
     sim_2(from="ou", sigma=info[["sigma"]], n_dataset = 4)
 })
 print("Simulate from OU")
 cbind(test_cases, t(ou_result))
 
-test_cases <- data.frame(sigma=c(0.00000001, 0.001, 0.01, 0.1, 1, sqrt(2), sqrt(5)))
+test_cases <- data.frame(sigma=c(0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, sqrt(2), sqrt(5)))
 bm_result <- apply(test_cases, 1, function(info) {
     sim_2(from="bm", sigma=info[["sigma"]], n_dataset = 4, n_obs = 200)
 })
 print("Simulate from BM")
 cbind(test_cases, t(bm_result))
+
 
