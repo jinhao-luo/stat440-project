579024471939a8edd07836a8e430e0e36bb4d941
Jinhao Luo <jayden_luo@hotmail.com>
2020-04-16 22:22:36 -0400

    Updated simulation script

--- code/sim1.R
+++ code/sim1.R
@@ -1,26 +1,30 @@
+setwd("code")
 source("sim1_omega_tau_multiple.R")
 
-all_cases <- read.csv("sim1_cases_new.csv", row.names=1)
+all_cases <- read.csv("sim1_cases_new.csv")
 # change the line below to your test cases
-my_ci <- 1:5
+my_ci <- 16:18
 
 my_cases <- all_cases[my_ci,]
+my_cases
 test_out_file <- paste0("sim1_case_new_", my_ci[1], "-", my_ci[length(my_ci)], ".RData")
 
 system(paste("osascript -e 'display notification \"", test_out_file,"STARTED\" with title \"STAT440\" sound name \"default\"'"))
 
 result <- apply(my_cases, 1, function(tc) {
     print(tc)
-    sim_1(beta0=as.numeric(tc[["beta0"]]), beta1=as.numeric(tc[["beta1"]]), 
+    sim_1(
+        beta0=as.numeric(tc[["beta0"]]), beta1=as.numeric(tc[["beta1"]]), 
+        omega=exp(-as.numeric(tc[["gamma"]])),
         n_dataset=as.numeric(tc[["n_dataset"]]), n_obs=as.numeric(tc[["n_obs"]]), 
         num_multistart=as.numeric(tc[["num_multistart"]]))
 })
-if (file.exists(test_out_file)) {
-    tmp_out_file = tempfile()
-    warning(paste("ERROR IN SIMULATION 1:", test_out_file, "exists... Saving to", tmp_out_file, "Instead. Please manually retrive file if desired."))
-    save(my_cases, result, file=tmp_out_file)
-} else{
+# if (file.exists(test_out_file)) {
+#     tmp_out_file = tempfile()
+#     warning(paste("ERROR IN SIMULATION 1:", test_out_file, "exists... Saving to", tmp_out_file, "Instead. Please manually retrive file if desired."))
+#     save(my_cases, result, file=tmp_out_file)
+# } else{
     save(my_cases, result, file=test_out_file)
-}
+# }
 
 system(paste("osascript -e 'display notification \"", test_out_file,"DONE\" with title \"STAT440\" sound name \"default\"'"))
--- code/sim1_result.R
+++ code/sim1_result.R
@@ -1,4 +1,5 @@
-vec_ci <- c("1-3") # TODO: add the remaining
+# vec_ci <- c("1-5","6-10", "11-15", "16-20","21-25", "26-30") # TODO: add the remaining
+vec_ci <- c("1-3","4-6","7-9", "10-12", "13-15", "16-18")
 
 # get data from RData files
 all_test_cases <- data.frame()
@@ -9,7 +10,10 @@ for (ci in vec_ci) {
 	vec_result <- c(vec_result, result)
 	all_test_cases <- rbind(all_test_cases, my_cases)
 	rmse <- cbind(my_cases, t(sapply(1:nrow(my_cases), function(i) {c(theta=result[[i]]$true_param, rmse=result[[i]]$rmse)})))
+	print(rmse)
 	all_rmse <- rbind(all_rmse, rmse)
 }
 names(vec_result) <- vec_ci
+all_rmse <- subset(all_rmse, select=-c(X))
 all_rmse
+write.csv(apply(all_rmse, 2,  as.numeric), "sim1_result.csv", row.names=FALSE)
