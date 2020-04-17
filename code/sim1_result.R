2a7888e53fb0066f7045370a192b2c958f29bb80
Wanxin Li <wanxinli@Wanxins-MacBook-Pro.local>
2020-04-16 23:47:17 -0400

    uploaded yaoliang tests

--- code/sim1_result.R
+++ code/sim1_result.R
@@ -1,5 +1,6 @@
 # vec_ci <- c("1-5","6-10", "11-15", "16-20","21-25", "26-30") # TODO: add the remaining
-vec_ci <- c("1-3","4-6","7-9", "10-12", "13-15", "16-18")
+# vec_ci <- c("1-3","4-6","7-9", "10-12", "13-15", "16-18")
+vec_ci <- c("19-21", "22-24", "25-27", "28-30", "31-33")
 
 # get data from RData files
 all_test_cases <- data.frame()
@@ -14,6 +15,6 @@ for (ci in vec_ci) {
 	all_rmse <- rbind(all_rmse, rmse)
 }
 names(vec_result) <- vec_ci
-all_rmse <- subset(all_rmse, select=-c(X))
+# all_rmse <- subset(all_rmse, select=-c(X))
 all_rmse
-write.csv(apply(all_rmse, 2,  as.numeric), "sim1_result.csv", row.names=FALSE)
+write.csv(apply(all_rmse, 2,  as.numeric), "ohno.csv", row.names=FALSE)
